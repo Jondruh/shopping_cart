7574f6f087131b7d6c4d4f1eace28172597a7c83
Jonathan Hurd <hurd.jonathan@gmail.com>
2024-02-09 13:42:00 -0600

    test cart not empty

--- client/src/components/Cart.test.jsx
+++ client/src/components/Cart.test.jsx
@@ -12,3 +12,11 @@ test("cart is empty", () => {
   const cart = screen.getByText("Your cart is empty");
   expect(cart).toBeInTheDocument();
 });
+
+test("cart is not empty", () => {
+  render(
+    <Cart cart={[{ id: 1, title: "Product 1", price: 100, quantity: 1 }]} />,
+  );
+  const cart = screen.getByRole("table");
+  expect(cart).toBeInTheDocument();
+});
